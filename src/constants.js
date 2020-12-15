6f3d77125000a6257597fdc126b572060679acfb
Leandro Corbalán <leandrocorbalan88@gmail.com>
2020-12-15 15:27:33 -0300

    changing HOST on constants

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const HOST = 'http://localhost:3000'
+const HOST = 'https://mercadopago-workshop-cert.herokuapp.com/'
 module.exports = {
   TEST_ACCESS_TOKEN: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398',
   TEST_INTEGRATOR_ID: 'dev_24c65fb163bf11ea96500242ac130004',
